e57b4836a48595d015de0df6e0ab2dce9897ed3f
Claudio Olivares <claudio.olivares@zentagroup.com>
2020-07-27 17:14:30 -0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        
+        Navbar
+      
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+         
+              Home <span className="sr-only">(current)</span>
+            
+          </li>
+          
+        </ul>
+      
+      </div>
+    </nav>
+                
+            </div>
+        )
+    }
+}
